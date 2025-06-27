cecba8de0b1ea2f47d95f81e6389dc583c51b2a5
Gowshik23 <gowshik127@gmail.com>
2025-06-27 09:29:11 +0530

    Add New

---
+++ src/org/sample/CharacterOccerence.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharacterOccerence 
+{
+public static void main(String[] args) {
+	String s="malayalam";
+	Map<Character, Integer> charcount=new LinkedHashMap<Character, Integer>();
+	for(int i=0;i<s.length();i++) {
+		char c=s.charAt(i);
+		if(charcount.containsKey(c))
+				{
+			charcount.put(c, charcount.get(c)+1);
+				}
+		else {
+			charcount.put(c, 1);
+		}
+	}
+	System.out.println(charcount);
+}
+}
